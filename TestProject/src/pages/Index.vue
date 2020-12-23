15213e63fbdcdba7dcd2f9f63c41980216b0518e
prakhar <praks.jain7@gmail.com>
2020-12-23 11:15:53 +0530

    Final Commit

--- TestProject/src/pages/Index.vue
+++ TestProject/src/pages/Index.vue
@@ -1,12 +1,17 @@
 <template>
-  <q-page class="">
+  <q-page class="layout-breakpoint">
+    <div class="text-grey row justify-end q-px-xl q-mx-xl" style="font-size: 3em">
+          <q-icon name="photo" />
+          <q-icon name="notifications" />
+          <q-icon name="person" />
+        </div>
     <div class="flex">
       <div class="content-area">
-      <q-card flat bordered class="my-card">
-        <q-card-section>
-          <faq-questions/>
-        </q-card-section>
-      </q-card>
+        <q-card flat class="my-card">
+          <q-card-section>
+            <faq-questions/>
+          </q-card-section>
+        </q-card>
       </div>
       <div class="my-width">
           <fundraising-block/>
@@ -32,6 +37,9 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+  @media only screen and (max-width: 1152px)
+    .layout-breakpoint
+      padding: .5rem
   .content-area
     text-overflow: ellipsis
     max-width: 50%
